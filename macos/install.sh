aedd1fefd66c40aad9f85724400b9532aca2274e
Michael Lai <drauren@vt.edu>
2022-04-05 21:26:33 -0400

    macos/ubuntu install.sh fixes

--- macos/install.sh
+++ macos/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-brew install kubectl
-brew install helm
-brew install tmux
-brew install derailed/k9s/k9s
+# General
 brew install --cask firefox
 brew install --cask visual-studio-code
 brew install --cask postman
+brew install tmux
+
+# k8s related
+brew install kubectl
+brew install helm
+brew install derailed/k9s/k9s
+brew install dive
